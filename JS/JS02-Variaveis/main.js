d420a1a0ab69c0c066660e623ae042a2eafcdf6c
Helloisa22 <helloisa.betriz44@gmail.com>
2024-05-08 21:46:13 -0300

    Aula JS 02 nao finalizada

---
+++ JS/JS02-Variaveis/main.js
@@ -0,0 +1,58 @@
+// Objeto console
+console.log("Hello World!");
+// Quando trabalho com texto coloco o mesmo dentro de aspas duplas ou simples
+
+// Variaveis
+// quero criar um espaço na memoria do computador chamado _felippe e dizer que esse espaço na memoria irá receber o valor 8
+var _felippe = 8;
+var _felippe = "Messi" // redeclarar uma variavel
+console.log(_felippe);
+
+let letsGo = 4 // não posso redeclarar
+letsGo = 6
+console.log(letsGo)
+
+console.clear();
+
+// const
+const PI = 3.1415;
+// PI = "5241"
+
+console.log(PI);
+
+console.clear()
+
+let NAN = 0 / 0;
+
+console.log(NAN)
+
+console.clear()
+
+let IOS = "Instituto da oportunidade social" // variavel IOS recebeu uma String
+
+console.log(IOS)
+
+console.log("Instituto " + " da " + "oportunidade " + " social");
+console.clear()
+
+var numero1 = 2
+var texto = "Eu tenho "
+var texto2 = " Reais"
+
+console.log(texto + numero1 + texto2);
+// concatenar = é possivel concatenar Strings ou concatenar variaveis
+
+console.clear()
+
+// Valores Booleanos
+
+var num1 = 5
+var num2 = 5
+// num1 é igual a num2
+// 1 é igual a 5 = true
+
+console.log(num1 == num2)
+
+var teste;
+
+console.log(teste)
