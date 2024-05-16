50a1f10d91df3265387d67f41ccafc21c1e59681
Helloisa22 <helloisa.betriz44@gmail.com>
2024-05-15 21:46:49 -0300

    Aula JS 05 finalizada

---
+++ JS/JS05-Repeticao/main.js
@@ -0,0 +1,32 @@
+// WHILE >> enquanto
+// Equanto 0 < 10 vou apresentar a mensagem " 0 ainda é menor que 10"
+// 0 < 10
+
+
+
+// do{
+//     debugger
+//     console.log(contador + " ainda é menor que 10 ")
+//     contador++;
+// }while(contador < 10)
+
+var contador = 10
+
+// while(contador < 10){
+//     console.log(contador + " ainda é menor que 10 ")
+//     contador++; // contador = contador + 1
+// }
+
+// if{
+//     for(){
+
+//     }
+// }else{
+//     while(){
+
+//     }
+// }
+
+for(var contador = 10; contador < 10; contador++){
+    console.log(contador + " ainda é menor que 10 ")
+}
