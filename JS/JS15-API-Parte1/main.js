7ee6a48a153b1469506f33e7a1ff61a9e955dda8
Helloisa22 <helloisa.betriz44@gmail.com>
2024-06-14 20:57:10 -0300

    JS 15 finalizada

--- JS/JS15-API-Parte1/main.js
+++ JS/JS15-API-Parte1/main.js
@@ -4,19 +4,25 @@ function getImage(e) {
     let url_api = document.getElementById('url_api').innerText;
     // console.log(url_api);
     // Função fetch para buscar dados na API
-    fetch(url_api, {method: 'GET',})
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            // console.log(data);
+    fetch('https://dog.ceo/api/breeds/image/random').then((response) => {
+            console.log(response);
+            return response.json(); // Receber o JSON
+        }).then((data) => {
+            console.log(data);
             document.getElementById('json_aqui').innerText =
                 JSON.stringify(data);
-            // console.log(data.message)
-            let imagem = `
-    <img class="rounded img-fluid" src="${data.message}" />
-    `;
+            console.log(data.message)
+            let imagem = `<img class="rounded img-fluid" src="${data.message}"/>`;
             // Insere a imagem no elemento HTML com id imagem_aqui
             document.querySelector('#imagem_aqui').innerHTML = imagem;
         });
 }
+// message
+// status
+
+// {"message":"https://images.dog.ceo/breeds/dachshund/puppy-1006024_640.jpg","status":"success"}
+
+// stringify >> Pega um JSON e transforma em um objeto
+// JSON >> Transforma um objeto em formato JSON
+
+fetch(url).then(receber o JSON).then(Tratar o JSON e mostrar as informações em tela para o usuario).catch(catch é para tratar erros retornados pela API)
